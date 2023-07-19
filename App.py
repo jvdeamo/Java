879e2fce3c0da5d458d70ee3e8eb850cba1e88cd
BatataRaiz <joaodeamo3@gmail.com>
2023-07-19 00:59:05 -0300

    Continuação exercícios

--- App.py
+++ App.py
@@ -2,31 +2,34 @@
 # Autor: João Victor Martins Deamo
 
 
-# Calculando a soma
+# Definindo a função delimitador
+def imprimir_delimitador(caractere, vezes):
+    print(caractere * vezes)
+imprimir_delimitador("-", 20)
+
+# 1 Calculando a soma
 def soma(j, v):
+    resultadoSoma = j + v
     print("A soma dos números digitados é:", resultadoSoma)
-    return j + v
-
-
+    # format print('A soma dos números digitados é: {}'.format(resultadoSoma))
+    # f print(f'A soma dos números digitados é: {resultadoSoma}')
+    return resultadoSoma
 resultadoSoma = soma(2, 3)
-# Usando format
-# print('A soma dos números digitados é: {}'.format(resultadoSoma))
-# Usando f
-# print(f'A soma dos números digitados é: {resultadoSoma}')
-print("----------------------------------------------")
 
+imprimir_delimitador("-", 20)
 
-# Calculando a média
+# 2 Calculando a média
 def media(j, v, d):
+    resultadoMedia = (j + v + d) / 3
     print("A média dos números digitados é:", resultadoMedia)
-    return (j + v + d) / 3
-
-
+    # Usando f-string
+    # print(f'A média dos números digitados é: {resultadoMedia:.2f}')
+    return resultadoMedia
 resultadoMedia = media(5, 4, 3)
-print("--------------------------------")
 
+imprimir_delimitador("-", 20)
 
-# Calculando o resto
+# 3 Calculando o resto
 def resto(j, v):
     if v == 0:
         return "Não é possível dividir por zero"
@@ -38,7 +41,7 @@ def resto(j, v):
             # print(f'O número {j} é par!')
             print("O resto da divisão é:", resultadoResto)
             # Usando f-string
-            # print(f'O resto da divisão é: {resultadoResto}')
+            # print(f'O resto da divisão é: {resultadoResto:.2f}')
             return resultadoResto
         if resultadoResto % 2 != 0:
             print("O número {} é ímpar!".format(j))
@@ -46,33 +49,30 @@ def resto(j, v):
             # print(f'O número {j} é ímpar!')
             print("O resto da divisão é:", resultadoResto)
             # Usando f-string
-            # print(f'O resto da divisão é: {resultadoResto}')
+            # print(f'O resto da divisão é: {resultadoResto:.2f}')
             return resultadoResto
-
-
 resultadoResto = resto(15, 6)
-print("--------------------------------")
 
+imprimir_delimitador("-", 20)
 
-# Calculando se o ano é bissexto
+# 4 Calculando o ano bissexto
 def ano_bissexto(ano):
+    resultadoAno = ano
     if ano % 4 == 0 and (ano % 100 != 0 or ano % 400 == 0):
         print("O ano {} é bissexto!".format(ano))
         # Usando f
         # print(f'O ano {ano} é bissexto!')
-        return
+        return resultadoAno
     else:
         print("O ano {} não é bissexto!".format(ano))
         # Usando f
         # print(f'O ano {ano} não é bissexto!')
         return
+resultadoAno = ano_bissexto(2020)
 
+imprimir_delimitador("-", 20)
 
-resultadoAno = ano_bissexto(2021)
-print("--------------------------------")
-
-
-# Calculando o fatorial
+# 5 Calculando o fatorial
 def fatorial(j):
     resultado = 1
     if j < 0:
@@ -88,9 +88,10 @@ def fatorial(j):
             # print(f"O fatorial de {j} é: {resultado}")
     return resultado
 fatorial(2)
-print("--------------------------------")
 
-# Calculando o primo
+imprimir_delimitador("-", 20)
+
+# 6 Calculando o número primo
 def primo(j):
     if j < 2:
         print("Não existe número primo menor que 2!")
@@ -108,3 +109,66 @@ def primo(j):
         # print(f'O número {j} é primo!')
         return
 primo(8)
+
+# 7	Escreva um programa que lê um número do usuário e imprime o número quadrado.
+def entrada():
+    try:
+        def input_7():
+            try:
+                j = float(input("Digite um número: "))
+                resultado = j**2
+                # Usando f-string:
+                print(f"{j} ao quadrado é: {resultado:.2f}.")
+                return resultado
+            except ValueError:
+                print("Digite apenas números!")
+                return input_7()
+
+        def input_8():
+            try:
+                j = float(input("Digite um número: "))
+                v = float(input("Digite outro número: "))
+                resultado = j + v
+                print(f"A soma dos números digitados é: {resultado:.2f}.")
+                return resultado
+            except ValueError:
+                print("Digite apenas números!")
+                return input_8()
+        
+        def input_9():
+            try:
+                # receber uma lista de números
+                lista = []
+                n = int(input("Digite a quantidade de números que deseja inserir: "))
+                for i in range(n):
+                    lista.append(int(input("Digite um número: ")))
+                resultado = min(lista)
+                print(f"O menor número digitado é: {resultado}.")
+                
+                return resultado
+            except ValueError:
+                print("Digite apenas números!")
+                return input_9()
+    except Exception:
+        print("Digite os valores corretamente!")
+        return entrada()
+
+    # 1 -> chamada delimitador
+    # 2 -> chamada função
+    imprimir_delimitador("-", 20)
+    input_7()
+    imprimir_delimitador("-", 20)
+    input_8()
+    imprimir_delimitador("-", 20)
+    input_9()
+
+
+entrada()
+# 8	Escreva um programa que lê dois números do usuário e imprime a soma.
+# 9	Escreva um programa que lê uma lista de números do usuário e imprime o menor número.
+# 10	Escreva um programa que lê uma lista de números do usuário e imprime a média dos números.
+# 11	Escreva um programa que lê uma lista de palavras do usuário e imprime as palavras que começam com a letra "A".
+# 12	Escreva um programa que lê uma lista de palavras do usuário e imprime as palavras que têm exatamente cinco letras.
+# 13	Escreva um programa que lê uma lista de palavras do usuário e imprime as palavras que são anagramas umas das outras.
+# 14	Escreva um programa que lê uma lista de números do usuário e imprime os números primos.
+# 15	Escreva um programa que lê uma lista de números do usuário e imprime os números pares.
