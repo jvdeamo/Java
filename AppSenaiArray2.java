ccbb5fb2f0c453e56b78a51e9437bb3f5523e080
João Victor Martins Deamo <joaodeamo3@gmail.com>
2023-08-03 11:46:26 -0300

    Update AppSenaiArray2.java

--- AppSenaiArray2.java
+++ AppSenaiArray2.java
@@ -53,218 +53,347 @@ class Vetores_2 {
         PrintStream out = System.out;
         out.println("\033[H\033[2J");
         Scanner scanner = new Scanner(System.in);
-        Vetores_2.Exercício11();
-        Vetores_2.Exercício12();
-        Vetores_2.Exercício13();
-        Vetores_2.Exercício14();
-        Vetores_2.Exercício15();
-        Vetores_2.Exercício16();
-        Vetores_2.Exercício17();
-        Vetores_2.Exercício18();
-
-        /*
-         * Vetores_2.Exercício15();
-         * Vetores_2.Exercício16();
-         * Vetores_2.Exercício17();
-         * Vetores_2.Exercício18();
-         * Vetores_2.Exercício19();
-         * Vetores_2.Exercício20();
-         */
-        scanner.close();
+        Vetores_2.Exercício20();
     }
+    /*
+     * Vetores_2.Exercício11();
+     * Vetores_2.Exercício12();
+     * Vetores_2.Exercício13();
+     * Vetores_2.Exercício14();
+     * Vetores_2.Exercício15();
+     * Vetores_2.Exercício16();
+     * Vetores_2.Exercício17();
+     * Vetores_2.Exercício18();
+     * Vetores_2.Exercício19();
+     */
 
-    public static void Exercício11() {
+    /*
+     * Vetores_2.Exercício15();
+     * Vetores_2.Exercício16();
+     * Vetores_2.Exercício17();
+     * Vetores_2.Exercício18();
+     * Vetores_2.Exercício19();
+     * Vetores_2.Exercício20();
+     * 
+     * scanner.close();
+     * }
+     * 
+     * public static void Exercício11() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 11");
+     * String[] vetor = new String[5];
+     * 
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.println("Digite um item para a lista de compras: ");
+     * vetor[i] = scanner.next();
+     * }
+     * out.print("Lista de compras: ");
+     * for (String lista : vetor) {
+     * out.print(lista + " ");
+     * }
+     * out.println();
+     * }
+     * 
+     * public static void Exercício12() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * Double[] vetor = new Double[7];
+     * String[] Semana = { "Segunda-feira", "Terça-feira", "Quarta-feira",
+     * "Quinta-feira", "Sexta-feira", "Sábado",
+     * "Domingo" };
+     * Double soma = 0.0;
+     * Double media = 0.0;
+     * out.println("Exercício 12");
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.println("Digite a temperatura média do dia " + (i + 1) + ": ");
+     * vetor[i] = scanner.nextDouble();
+     * soma = soma + vetor[i];
+     * }
+     * out.println("Temperaturas diárias: ");
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("%s: %.2fºC. \n", Semana[i], vetor[i]);
+     * }
+     * media = soma / vetor.length;
+     * out.printf("A temperatura média da semana foi de: %.2fºC", media);
+     * out.println("\n");
+     * }
+     * 
+     * public static void Exercício13() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 13");
+     * int numero = 0;
+     * int tentativas = 0;
+     * boolean acertou = false;
+     * Random aleatorio = new Random();
+     * int numeroAleatorio = aleatorio.nextInt(100);
+     * out.println("Tente adivinhar o número secreto entre 1 e 100: ");
+     * out.println(numeroAleatorio);
+     * while (acertou == false) {
+     * numero = scanner.nextInt();
+     * if (numero == numeroAleatorio) {
+     * acertou = true;
+     * } else if (numero > numeroAleatorio) {
+     * out.println("O número secreto é menor que " + numero);
+     * } else {
+     * out.println("O número secreto é maior que " + numero);
+     * }
+     * tentativas = tentativas + 1;
+     * }
+     * out.printf("Parabéns, você acertou o número secreto [%d] em %d tentativas.\n"
+     * , numeroAleatorio, tentativas);
+     * out.println();
+     * }
+     * 
+     * public static void Exercício14() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 14");
+     * Double[] vetor = new Double[5];
+     * Double soma = 0.0;
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.println("Digite a quantidade de horas gastas na tarefa " + (i + 1) +
+     * ": ");
+     * vetor[i] = scanner.nextDouble();
+     * soma = soma + vetor[i];
+     * }
+     * out.println("Horas gastas em cada tarefa: ");
+     * int i = 0;
+     * for (double horasG : vetor) {
+     * out.printf("Tarefa %d: %.2f horas. \n", i, horasG);
+     * i = i + 1;
+     * }
+     * out.printf("Total de horas gastas: %.2f horas.", soma);
+     * out.println("\n");
+     * }
+     * 
+     * public static void Exercício15() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 15");
+     * String[] vetor = new String[5];
+     * String[] Ordem = { "primeiro", "segundo", "terceiro", "quarto", "quinto" };
+     * String[] sorteados = new String[3];
+     * Random aleatorio = new Random();
+     * int numeroAleatorio = aleatorio.nextInt(5);
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("Digite o nome do %s convidado: ", Ordem[i]);
+     * vetor[i] = scanner.next();
+     * }
+     * out.print("Convidados - ");
+     * for (String convidados : vetor) {
+     * out.printf("%s, ", convidados);
+     * }
+     * out.print(".\n");
+     * for (int i = 0; i < sorteados.length; i++) {
+     * numeroAleatorio = aleatorio.nextInt(5);
+     * sorteados[i] = vetor[numeroAleatorio];
+     * /*
+     * aqui ele atribui o valor sorteado a cada posição do vetor, por exemplo:
+     * se o numeroAleatorio for 1
+     * sorteado[0] = vetor[numeroAleatorio];
+     * sorteado[1] = vetor[numeroAleatorio];
+     * sorteado[2] = vetor[numeroAleatorio];
+     * 
+     * }
+     * out.print("Convidados sorteados - ");
+     * for (String sorteadosNomes : sorteados) {
+     * out.printf("%s, ", sorteadosNomes);
+     * }
+     * out.println();
+     * }
+     * 
+     * public static void Exercício16() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 16");
+     * float[] vetor = new float[7];
+     * String[] Semana = { "Segunda-feira", "Terça-feira", "Quarta-feira",
+     * "Quinta-feira", "Sexta-feira", "Sábado",
+     * "Domingo" };
+     * float soma = 0.0f;
+     * float media = 0.0f;
+     * 
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.println("Digite a quantidade de vendas de " + (Semana[i]) + ": ");
+     * vetor[i] = scanner.nextFloat();
+     * soma = soma + vetor[i];
+     * }
+     * 
+     * out.println("Vendas diárias: ");
+     * int dia = 1;
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("%s: %.1f vendas. \n", Semana[i], vetor[i]);
+     * dia = dia + 1;
+     * }
+     * 
+     * media = soma / vetor.length;
+     * 
+     * // Saídas
+     * out.printf("A média de vendas diárias da semana foi de: %.1f vendas.",
+     * media);
+     * out.println("\n");
+     * }
+     * 
+     * public static void Exercício17() {
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 17");
+     * String[] vetor = new String[5];
+     * int[] vetor2 = new int[5];
+     * String[] amigos = { "da onça", "do peito", "da barriga", "do coração",
+     * "da alma" };
+     * 
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("Digite o nome do amigo %s: ", (amigos[i]));
+     * vetor[i] = scanner.next();
+     * out.printf("Quebra-cabeças resolvidos pelo %s: ", vetor[i]);
+     * vetor2[i] = scanner.nextInt();
+     * }
+     * 
+     * // Saídas
+     * out.println("Amigos e quebra-cabeças resolvidos: ");
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("%s %s: %d quebra-cabeças. \n", vetor[i], amigos[i], vetor2[i]);
+     * }
+     * out.println();
+     * }
+     * 
+     * public static void Exercício18() {
+     * // Declarações
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 18");
+     * 
+     * // Inicializações
+     * Double[] vetor = new Double[10];
+     * String[] Semana = { "Segunda-feira", "Terça-feira", "Quarta-feira",
+     * "Quinta-feira", "Sexta-feira", "Sábado",
+     * "Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira" };
+     * Double maiorVariacao = 0.0;
+     * Double variacao = 0.0;
+     * 
+     * out.
+     * println("Relatório de variação de preço de ações no período 10 dias, informe os dados necessários: "
+     * );
+     * 
+     * // Entradas
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("Digite o preço da ação de %s: ", (Semana[i]));
+     * vetor[i] = scanner.nextDouble();
+     * }
+     * 
+     * // Consulta preços das ações
+     * out.println("Preços da ação: ");
+     * int dia = 1;
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("%s: R$%.4f. \n", Semana[i], vetor[i]);
+     * dia = dia + 1;
+     * }
+     * 
+     * // Processamento maior variação de preço
+     * for (int i = 0; i < vetor.length - 1; i++) {
+     * variacao = vetor[i + 1] - vetor[i];
+     * if (variacao > maiorVariacao) {
+     * maiorVariacao = variacao;
+     * }
+     * }
+     * 
+     * // Saídas
+     * out.
+     * printf("A maior variação de preço entre dois dias consecutivos foi de: R$%.4f."
+     * , maiorVariacao);
+     * out.println("\n");
+     * }
+     * 
+     * public static void Exercício19() {
+     * // Declarações
+     * PrintStream out = System.out;
+     * Scanner scanner = new Scanner(System.in);
+     * out.println("Exercício 19");
+     * 
+     * // Inicializações
+     * String[] vetor = new String[5];
+     * float[] vetor2 = new float[5];
+     * String[] Ordem = { "primeiro", "segundo", "terceiro", "quarto", "quinto" };
+     * String nomeVencedor = vetor[0];
+     * float menorTempo = Float.MAX_VALUE; // Inicializa com o primeiro valor do
+     * array vetor2
+     * 
+     * out.println("Relatório de tempo de natação, informe os dados necessários: ");
+     * 
+     * // Entradas
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("Digite o nome do %s nadador: ", (Ordem[i]));
+     * vetor[i] = scanner.next();
+     * out.printf("Digite o tempo de natação do %s: ", vetor[i]);
+     * vetor2[i] = scanner.nextFloat();
+     * }
+     * 
+     * // Consulta tempos de natação
+     * out.println("Tempos de natação: ");
+     * for (int i = 0; i < vetor.length; i++) {
+     * out.printf("%s: %.3f segundos. \n", vetor[i], vetor2[i]);
+     * }
+     * 
+     * // Processamento menor tempo de natação
+     * // Processamento menor tempo de natação
+     * for (int i = 0; i < vetor.length; i++) {
+     * if (vetor2[i] < menorTempo)
+     * menorTempo = vetor2[i];
+     * nomeVencedor = vetor[i];
+     * }
+     * 
+     * // Saídas
+     * out.printf("O nadador vencedor foi %s com o tempo de %.3f segundos.",
+     * nomeVencedor, menorTempo);
+     * out.println("\n");
+     * }
+     */
+    public static void Exercício20() {
+        // Declarações
         PrintStream out = System.out;
         Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 11");
-        String[] vetor = new String[5];
+        out.println("Exercício 20");
 
-        for (int i = 0; i < vetor.length; i++) {
-            out.println("Digite um item para a lista de compras: ");
-            vetor[i] = scanner.next();
-        }
-        out.print("Lista de compras: ");
-        for (String lista : vetor) {
-            out.print(lista + " ");
-        }
-        out.println();
-    }
-
-    public static void Exercício12() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        Double[] vetor = new Double[7];
-        String[] Semana = { "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado",
-                "Domingo" };
-        Double soma = 0.0;
-        Double media = 0.0;
-        out.println("Exercício 12");
-        for (int i = 0; i < vetor.length; i++) {
-            out.println("Digite a temperatura média do dia " + (i + 1) + ": ");
-            vetor[i] = scanner.nextDouble();
-            soma += vetor[i];
-        }
-        out.println("Temperatudas diárias: ");
-        for (int i = 0; i < vetor.length; i++) {
-            out.printf("%s: %.2fºC. \n", Semana[i], vetor[i]);
-        }
-        media = soma / vetor.length;
-        out.printf("A temperatura média da semana foi de: %.2fºC", media);
-        out.println("\n");
-    }
-
-    public static void Exercício13() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 13");
-        int numero = 0;
-        int tentativas = 0;
-        boolean acertou = false;
-        Random aleatorio = new Random();
-        int numeroAleatorio = aleatorio.nextInt(100);
-        out.println("Tente adivinhar o número secreto entre 1 e 100: ");
-        out.println(numeroAleatorio);
-        while (acertou == false) {
-            numero = scanner.nextInt();
-            if (numero == numeroAleatorio) {
-                acertou = true;
-            } else if (numero > numeroAleatorio) {
-                out.println("O número secreto é menor que " + numero);
-            } else {
-                out.println("O número secreto é maior que " + numero);
-            }
-            tentativas = tentativas + 1;
-        }
-        out.printf("Parabéns, você acertou o número secreto [%d] em %d tentativas.\n", numeroAleatorio, tentativas);
-        out.println();
-    }
-
-    public static void Exercício14() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 14");
-        Double[] vetor = new Double[5];
-        Double soma = 0.0;
-        for (int i = 0; i < vetor.length; i++) {
-            out.println("Digite a quantidade de horas gastas na tarefa " + (i + 1) + ": ");
-            vetor[i] = scanner.nextDouble();
-            soma += vetor[i];
-        }
-        out.println("Horas gastas em cada tarefa: ");
-        int i = 0;
-        for (double horasG : vetor) {
-            out.printf("Tarefa %d: %.2f horas. \n", i, horasG);
-            i = i + 1;
-        }
-        out.printf("Total de horas gastas: %.2f horas.", soma);
-        out.println("\n");
-    }
-
-    public static void Exercício15() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 15");
-        String[] vetor = new String[5];
-        String[] Ordem = { "Primeiro", "Segundo", "Terceiro", "Quarto", "Quinto" };
-        String[] sorteados = new String[3];
-        Random aleatorio = new Random();
-        int numeroAleatorio = aleatorio.nextInt(5);
-        for (int i = 0; i < vetor.length; i++) {
-            out.printf("Digite o nome do %s convidado: ", Ordem[i]);
-            vetor[i] = scanner.next();
-        }
-        out.print("Convidados - ");
-        for (String convidados : vetor) {
-            out.printf("%s, ", convidados);
-        }
-        out.print(".\n");
-        for (int i = 0; i < sorteados.length; i++) {
-            numeroAleatorio = aleatorio.nextInt(5);
-            sorteados[i] = vetor[numeroAleatorio];
-        }
-        out.print("Convidados sorteados - ");
-        for (String sorteadosNomes : sorteados) {
-            out.printf("%s, ", sorteadosNomes);
-        }
-        out.println();
-    }
-
-    public static void Exercício16() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 16");
+        // Inicializações
         float[] vetor = new float[7];
-        float soma = 0.0f;
-        float media = 0.0f;
+        String[] Nomes = new String[7];
+        float maiorCaloria = 0.0f;
+        float caloria = 0.0f;
+        String nomeMaiorCaloria = "";
 
+        out.println("Relatório de calorias consumidas no período de 7 dias, informe os dados necessários: ");
+
+        // Entradas
         for (int i = 0; i < vetor.length; i++) {
-            out.println("Digite a quantidade de vendas do dia " + (i + 1) + ": ");
+            out.printf("Digite o nome do paciente %d: ", (i + 1));
+            Nomes[i] = scanner.next();
+            out.printf("Digite a quantidade de calorias consumidas por %s: ", (Nomes[i]));
             vetor[i] = scanner.nextFloat();
-            soma = soma + vetor[i];
         }
 
-        out.println("Vendas diárias: ");
+        // Consulta calorias consumidas
+        out.println("Calorias consumidas: ");
         int dia = 1;
-        for (float venda : vetor) {
-            out.printf("Dia %d: %.2f vendas. \n", dia, venda);
+        for (int i = 0; i < vetor.length; i++) {
+            out.printf("%s: %.1f calorias. \n", Nomes[i], vetor[i]);
             dia = dia + 1;
         }
 
-        media = soma / vetor.length;
-        out.printf("A média de vendas diárias da semana foi de: %f vendas.", media);
-        out.println("\n");
-    }
-
-    public static void Exercício17() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 17");
-        String[] vetor = new String[5];
-        int[] vetor2 = new int[5];
-
+        // Processamento maior caloria
         for (int i = 0; i < vetor.length; i++) {
-            out.printf("Digite o nome do %d amigo: ", (i + 1));
-            vetor[i] = scanner.next();
-            out.printf("Digite a quantidade de quebra-cabeças resolvidos pelo %s: ", vetor[i]);
-            vetor2[i] = scanner.nextInt();
-        }
-
-        out.println("Amigos e quebra-cabeças resolvidos: ");
-        for (int i = 0; i < vetor.length; i++) {
-            out.printf("%s: %d quebra-cabeças. \n", vetor[i], vetor2[i]);
-        }
-        out.println();
-    }
-
-    public static void Exercício18() {
-        PrintStream out = System.out;
-        Scanner scanner = new Scanner(System.in);
-        out.println("Exercício 18");
-        Double[] vetor = new Double[10];
-        Double maiorVariacao = 0.0;
-        Double variacao = 0.0;
-
-        for (int i = 0; i < vetor.length; i++) {
-            out.printf("Digite o preço da ação no dia %d: ", (i + 1));
-            vetor[i] = scanner.nextDouble();
-        }
-
-        out.println("Preços da ação: ");
-        int dia = 1;
-        for (Double preco : vetor) {
-            out.printf("Dia %d: R$%.2f. \n", dia, preco);
-            dia = dia + 1;
-        }
-
-        for (int i = 0; i < vetor.length - 1; i++) {
-            variacao = vetor[i + 1] - vetor[i];
-            if (variacao > maiorVariacao) {
-                maiorVariacao = variacao;
+            caloria = vetor[i];
+            if (caloria > maiorCaloria) {
+                maiorCaloria = caloria;
+                nomeMaiorCaloria = Nomes[i];
             }
         }
 
-        out.printf("A maior variação de preço entre dois dias consecutivos foi de: R$%.2f.", maiorVariacao);
+        // Saídas
+        out.printf("A pessoa que consumiu a maior quantidade de calorias em um único dia foi %s com %.1f calorias.",
+                nomeMaiorCaloria, maiorCaloria);
         out.println("\n");
     }
-
 }
