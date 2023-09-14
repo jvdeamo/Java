f16c5800777a915b6bd9df274f3c337c6d1eb7d0
João Victor Martins Deamo <joaodeamo3@gmail.com>
2023-09-14 08:22:50 -0300

    Update AppSenai.py

--- python_src/AppSenai.py
+++ python_src/AppSenai.py
@@ -9,7 +9,7 @@ Autor: João Victor Martins Deamo
 Date: 12/09/2023
 Time: 08:20
 IDE: Visual Studio Code
-Session Duration: 08:20 - 
+Session Duration: 08:20 - 11:30
 Subject: Python
 Version: 1.1
 Senai - Desenvolvimento de Sistemas
